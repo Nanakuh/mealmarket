f7febe1e5216ce6e2a280575eb375e3410b76e96
Ana Gil <anagilrodrig@gmail.com>
2024-06-01 20:10:12 +0200

    Update form

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -1,30 +1,30 @@
 function Contact() {
   return <div><section className="info">
 
-  <h2 className="title-contact">CONTACT</h2>
+  <h2 className="text-emerald-800 font-black tracking-wider text-2xl flex justify-center my-10"  >CONTACT</h2>
   <article>
-      <p className="description-contact">From this form you can contact us and ask us any questions.
+      <p className="ml-7 mb-4">From this form you can contact us and ask us any questions.
           We encourage you to write to us!!!</p>
-      <p className="required">
+      <p className="ml-7 mb-4">
       All fields marked with <span className="asterisc">*</span> are mandatory.
       </p>
   </article>
 </section>
-<section className="htmlForm">
+<section className="ml-7 mb-4">
   <htmlForm className="htmlFormulary" action="https://htmlFormspree.io/f/xoqozyjz" method="post">
-      <label className="field" htmlFor="name">Full name<span className="asterisc"> *</span></label>
+      <label className="text-emerald-800 font-black mr-2" htmlFor="name">Full name<span className="asterisc"> *</span></label>
       <input className="input" type="text" id="name" name="complete name" placeholder="Name..."
           title="Escribe tu nombre y apellidos" />
-      <label className="field" htmlFor="email">Email<span className="asterisc"> *</span></label>
+      <label className="text-emerald-800 font-black ml-5 mr-2" htmlFor="email">Email<span className="asterisc"> *</span></label>
       <input className="input" type="email" id="email" name="email" placeholder="email@mail.com"
           title="Escribe tu email de contacto" />
-      <label className="field" htmlFor="phone">Telephone </label>
+      <label className="text-emerald-800 font-black ml-5 mr-2" htmlFor="phone">Telephone </label>
       <input className="input" type="tel" id="phone" name="telephone" placeholder="Ej: 123456789"
           title="Escribe tu teléfono" />
-      <label className="field" htmlFor="notes">Message <span className="asterisc"> *</span></label>
+      <label className="text-emerald-800 font-black ml-5 mr-2" htmlFor="notes">Message <span className="asterisc"> *</span></label>
       <input className="input--text" type="text" id="notes" name="comments" placeholder="What do you need?"
           title="Escribe tu petición" />
-      <input className="submit" type="submit" value="SEND" />
+      <input className="text-emerald-800 font-black ml-5 mr-2" type="submit" value="SEND" />
   </htmlForm>
 </section></div>;
 }
